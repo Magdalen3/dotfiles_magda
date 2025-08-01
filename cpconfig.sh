770601c7b336af30d6331e3ec1c890cbe22f8d5e
magdalaptop <delriomagdalene@gmail.com>
2025-08-01 02:24:41 -0300

    script en bash para copiar los dotfiles

---
+++ cpconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+configs=("nvim" "sway" "swaylock" "waybar")
+
+dotfiles_dir="$HOME/dotfiles"
+if [ ! -d "$dotfiles_dir" ]; then
+    mkdir -p "$dotfiles_dir"
+    echo "carpeta dotfiles creada en $dotfiles_dir"
+fi
+
+for config in "${configs[@]}"; do
+    config_path="$HOME/.config/$config"
+    
+    if [ -f "$config_path" ] || [ -d "$config_path" ]; then
+        cp -r "$config_path" "$dotfiles_dir/"
+        echo "copiado: $config"
+    else
+        echo "no encontrado: $config_path"
+    fi
+done
+
+echo "listop"
